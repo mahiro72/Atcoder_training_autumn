0760143af50ee1aab6bfe78d621a016f974baedf
mahiro <mahiro.yoshie@gmail.com>
2021-10-07 00:40:50 +0900

    score 0/2

--- 2021_10/5/tr10/1.py
+++ 2021_10/5/tr10/1.py
@@ -2,13 +2,13 @@ H,W = map(int,input().split())
 N = int(input())
 A = list(map(int,input().split()))
 
-tmp = ''
+tmp = []
 for i,a in enumerate(A):
-    tmp+=str(i+1)*a
+    for j in range(a):
+        tmp.append(i+1)
 
 ans = []
 for i in range(H):
     x = tmp[i*W:(i+1)*W]
-    ans.append(x if i%2==0 else x[::-1])
-
-for i in ans:print(*[x for x in i])
+    if i%2==1:x.reverse()
+    print(*x)
